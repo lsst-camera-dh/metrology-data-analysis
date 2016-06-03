7af87adb3f8167fc0ee117fc6b8a1cb75a174baf
Homer A. Neal Jr <homer@rhel6-64k.slac.stanford.edu>
2016-06-02 18:34:49 -0700

    modified to work with the OGP upload job harness

--- harnessed_jobs/absolute_height/v0/producer_absolute_height.py
+++ harnessed_jobs/absolute_height/v0/producer_absolute_height.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
 import sys
+import os
 import siteUtils
-import metUtils
+#import metUtils
 from absoluteHeightTask import absoluteHeightTask
+
+os.system("rsync -r LSSTuser@172.17.100.2:jobHarness/jh_stage/e2v-CCD/An-e2v-CCD/\* /home/LSSTuser/jobHarness/jh_stage/e2v-CCD/")
 
 sensor_id = siteUtils.getUnitId()
 ccd_vendor = siteUtils.getCcdVendor()
 
 # Find the OGP metrology scan data
-met_file = siteUtils.dependency_glob('*_AbsHeight*.DAT',
+#met_file = siteUtils.dependency_glob('*/*/*_AbsHeight*.DAT',
+direcs = siteUtils.dependency_glob('*',
 		jobname=siteUtils.getProcessName('OGP_Dimensional_Metrology_Upload'),description='OGP Absolute Height scan:')
+print "direcs:"
+print direcs
+print "------------------------------"
 
-met_file = met_file[0]  # siteUtils returns met_file as a list with one member;
+dimdir = ""
+for line in direcs :
+    if "DimensionalMetrology" in line :
+        dimdir = line
+
+print "dimensional metrology directory = %s" % dimdir
+
+
+met_file = ""
+
+for root, dirs, met_files in os.walk(dimdir):
+    for file in met_files:
+        if file.endswith(".DAT"):
+             met_file=os.path.join(root, file)
+
+#met_file = met_file[0]  # siteUtils returns met_file as a list with one member;
 			# here take the first (and only) member
+
+print "met_file = %s" % met_file
 
 # Below dtype is always 'OGP', i.e., for this task the source of the data
 # is always the OGP scanner at BNL
--- harnessed_jobs/flatness/v0/producer_flatness.py
+++ harnessed_jobs/flatness/v0/producer_flatness.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python
+import sys
+import os
 import siteUtils
 import metUtils
 from flatnessTask import flatnessTask
+
+os.system("rsync -r LSSTuser@172.17.100.2:jobHarness/jh_stage/e2v-CCD/An-e2v-CCD/\* /home/LSSTuser/jobHarness/jh_stage/e2v-CCD/")
 
 sensor_id = siteUtils.getUnitId()
 ccd_vendor = siteUtils.getCcdVendor()
 
 # Find the OGP metrology scan data
-met_file = siteUtils.dependency_glob('*_Flatness*.DAT',jobname=siteUtils.getProcessName('OGP_Dimensional_Metrology_Upload'),description='OGP Flatness scan:')
+#flat_file = siteUtils.dependency_glob('*/*/*_AbsHeight*.DAT',
+direcs = siteUtils.dependency_glob('*',
+                                   jobname=siteUtils.getProcessName('OGP_Dimensional_Metrology_Upload'),description='OGP Absolute Height scan:')
+print "direcs:"
+print direcs
+print "------------------------------"
 
-met_file = met_file[0]  # siteUtils returns met_file as a list with one member;
-                        # here take the first (and only) member
+dimdir = ""
+for line in direcs :
+    if "DimensionalMetrology" in line :
+        dimdir = line
+
+print "dimensional metrology directory = %s" % dimdir
+
+
+flat_file = ""
+
+for root, dirs, flat_files in os.walk(dimdir):
+    for file in flat_files:
+        if file.endswith(".DAT"):
+             flat_file=os.path.join(root, file)
+
+#flat_file = flat_file[0]  # siteUtils returns flat_file as a list with one member;
+    # here take the first (and only) member
+
+print "flat_file = %s" % flat_file
 
 # The dtype below indicates the source of the data, which is always OGP
 # for sensors measured at BNL
-flatnessTask(sensor_id, met_file, dtype='OGP', pickle_file='flatness.pickle')
+flatnessTask(sensor_id, flat_file, dtype='OGP', pickle_file='flatness.pickle')

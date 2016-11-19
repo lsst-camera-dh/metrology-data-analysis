1e6765dd4552704ad9a3ce29a0271b8b9ff0e973
Seth Digel <digel@stanford.edu>
2016-11-19 12:12:00 -0800

    Homer's updates

--- harnessed_jobs/flatness_ts5/v0/validator_flatness_ts5.py
+++ harnessed_jobs/flatness_ts5/v0/validator_flatness_ts5.py
@@ -6,11 +6,17 @@ from MetrologyData import md_factory
 
 producer = 'SR-MET-07'
 testtype = 'FLATNESS'
+
+global raftname
+raftname = siteUtils.getUnitId()
+
 results = metUtils.aggregate_filerefs_ts5(producer, testtype)
 
 raftData = md_factory.load('flatness_ts5.pickle')
 peak_valley_95 = raftData.quantiles['0.975'] - raftData.quantiles['0.025']
 results.append(lcatr.schema.valid(lcatr.schema.get('raft_flatness'),
+                                  residual_025=raftData.quantiles['0.025'],
+                                  residual_975=raftData.quantiles['0.975'],
                                   peak_valley_95=peak_valley_95))
 
 results.append(siteUtils.packageVersions())

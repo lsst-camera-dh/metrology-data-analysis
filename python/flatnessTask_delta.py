82da213591bc47e3103d0b87ef99e5a266cc6a25
Seth Digel <digel@stanford.edu>
2017-01-06 00:45:08 -0800

    Modified to always compare the two room-temperature scans

--- python/flatnessTask_delta.py
+++ python/flatnessTask_delta.py
@@ -3,10 +3,10 @@ import MetrologyData as metData
 from MetrologyData import md_factory
 
 def flatnessTask_delta(raft_id, infiles, dtype='OGP', pickle_file=None):
-    # This is modified from flatnessTask to accept a list of data files as
-    # input.  # The script processes each file to evaluate the temperature
-    # and standard deviation of the residuals, # then look the differences
-    # of the results for warm, cold, and warm again
+    # This is modified from flatnessTask to accept a list of two data files as
+    # input, the two room-temperature scans for a TS5 run, and evaluate the
+    # change in flatness between the two scans.
+    
     # This routine implicitly assumes that the scan grids are exactly
     # the same for the different data sets.  It does keep track of
     # which scan points are missing in the various data sets.
@@ -15,54 +15,11 @@ def flatnessTask_delta(raft_id, infiles, dtype='OGP', pickle_file=None):
     # subtract them, and look at the difference residuals: histogram,
     # quantiles, plots."
 
-    # Process each file, evaluating temperature, time, and standard
-    # deviation of flatness
-
-    stdev = []
-    temperature = []
-    nsigma = 4
-
-    for infile in infiles:
-        raftData = md_factory.create([infile], dtype=dtype)
-        # Fit and set the reference plane to the LSF to the raft surface
-        # points.
-        #
-        raftData.set_ref_plane(raftData.sensor.xyzPlane_fit(), zoffset=0)
-        dz = raftData.resids
-
-        # Apply nsigma clipping to evaluate the standard deviation
-        mean = np.mean(dz)
-        std = np.std(dz)
-        index = np.where((dz > mean-nsigma*std) & (dz < mean+nsigma*std))
-        stdev.append(np.std(dz[index]))
-
-        # Parse the file name to get the temperature and time stamp
-        pieces = infile.split('_')
-        nelem = len(pieces)
-        loc = pieces[nelem-1].find('C')
-        temperature.append(pieces[nelem-1][0:loc])
-
-    # Find the 'best' (least standard deviation, after clipping) scans at the warmest and
-    # coldest temperatures
-    temp = np.array(temperature).astype(float)
-    loc = np.where((temp == np.max(temp)))[0]
-    std = np.array(stdev)
-    pos = np.where(std[loc] == np.min(std[loc]))[0]
-    locWarm = loc[pos[0]]
-    loc = np.where((temp == np.min(temp)))[0]
-    pos = np.where(std[loc] == np.min(std[loc]))[0]
-    locCold = loc[pos[0]]
-
-    # print("locWarm:  ", locWarm)
-    # print("locCold:  ", locCold)
-
-    raftDataDelta = md_factory.create([infiles[locWarm], infiles[locCold]], dtype=dtype)
+    raftDataDelta = md_factory.create(infiles, dtype=dtype)
 
     # For positions that are in common in the scans evaluate the difference in z
     raftDataDelta.set_ref_plane(raftDataDelta.sensor.xyzPlane_fit(), zoffset=0)
     dzDelta = raftDataDelta.resids
-    # print("flatness resid:  ", np.std(dzDelta))
-
     #
     # Write residual points relative to LSF surface.
     #

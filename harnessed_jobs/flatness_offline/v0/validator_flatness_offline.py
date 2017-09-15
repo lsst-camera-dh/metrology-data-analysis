a7494f04d314e96616016edcf81d21a6b5be214b
Seth Digel <digel@stanford.edu>
2017-09-15 00:18:55 -0700

    Fixed unbalanced () in validator

--- harnessed_jobs/flatness_offline/v0/validator_flatness_offline.py
+++ harnessed_jobs/flatness_offline/v0/validator_flatness_offline.py
@@ -13,7 +13,7 @@ peak_valley_95 = sensorData.quantiles['0.975'] - sensorData.quantiles['0.025']
 flatnesshalfband_95 = peak_valley_95/2.
 
 results.append(lcatr.schema.valid(lcatr.schema.get('flatness_sensor'),
-                                  peak_valley_95=peak_valley_95))
+                                  peak_valley_95=peak_valley_95,
                                   flatnesshalfband_95=flatnesshalfband_95))
 
 results.extend(siteUtils.packageVersions())
